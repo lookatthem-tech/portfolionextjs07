f030723b80fd59534f178c97f4c65492d330661a
lookatthem-tech <112528495+lookatthem-tech@users.noreply.github.com>
2025-07-21 11:40:39 -0400

    Update theme.config.tsx

--- theme.config.tsx
+++ theme.config.tsx
@@ -4,7 +4,7 @@ import { DocsThemeConfig } from 'nextra-theme-docs'
 const config: DocsThemeConfig = {
   logo: <span>Ryan Rader</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/lookatthem-tech/portfolionextjs07',
   },
   chat: {
     link: 'https://discord.com',
